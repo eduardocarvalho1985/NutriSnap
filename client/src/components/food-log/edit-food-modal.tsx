1e27254c07ebd051e4529c93202b697c396204f5
Eduardo de Carvalho <eduardo.carvalho1985@gmail.com>
2025-05-16 17:07:27 +0000

    button label change

--- client/src/components/food-log/edit-food-modal.tsx
+++ client/src/components/food-log/edit-food-modal.tsx
@@ -291,7 +291,7 @@ export function EditFoodModal({
                   <Loader2Icon className="mr-2 h-4 w-4 animate-spin" />
                   Salvando...
                 </>
-              ) : "Atualizar Alimento"}
+              ) : "Atualizar"}
             </Button>
             
             <div className="grid grid-cols-2 gap-3 w-full">
